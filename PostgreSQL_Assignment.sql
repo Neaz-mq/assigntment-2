53bd49cb625d3c7f0fddfa5508c9b326d616cf02
Neaz-mq <neazmorshed666@gmail.com>
2025-05-25 21:58:45 +0600

    problem solution done

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -42,6 +42,9 @@ INSERT INTO species (common_name, scientific_name, discovery_date, conservation_
 ('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
 
 
+SELECT * FROM species;
+
+
 -- Sightings
 
 INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES
@@ -51,9 +54,79 @@ INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VA
 (1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
 
 
+SELECT * FROM sightings;
+
+
+
 -- Problem 1
 INSERT INTO rangers (name, region)
 VALUES ('Derek Fox', 'Coastal Plains');
+
+
+-- Problem 2 
+SELECT COUNT(DISTINCT species_id) AS unique_species_count
+FROM sightings;
+
+
+-- Problem 3
+SELECT * FROM sightings
+WHERE location ILIKE '%Pass%';
+
+
+-- Problem 4
+SELECT r.name, COUNT(s.sighting_id) AS total_sightings
+FROM rangers r
+LEFT JOIN sightings s ON r.ranger_id = s.ranger_id
+GROUP BY r.name
+ORDER BY r.name;
+
+
+-- Problem 5
+SELECT s.common_name
+FROM species s
+LEFT JOIN sightings si ON s.species_id = si.species_id
+WHERE si.species_id IS NULL;
+
+
+-- Problem 6
+SELECT sp.common_name, si.sighting_time, r.name
+FROM sightings si
+JOIN species sp ON si.species_id = sp.species_id
+JOIN rangers r ON si.ranger_id = r.ranger_id
+ORDER BY si.sighting_time DESC
+LIMIT 2;
+
+
+-- Problem 7
+UPDATE species
+SET conservation_status = 'Historic'
+WHERE discovery_date < '1800-01-01';
+
+
+-- Problem 8
+SELECT sighting_id,
+    CASE 
+        WHEN EXTRACT(HOUR FROM sighting_time) < 12 THEN 'Morning'
+        WHEN EXTRACT(HOUR FROM sighting_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+        ELSE 'Evening'
+    END AS time_of_day
+FROM sightings;
+
+
+-- Problem 9
+DELETE FROM rangers
+WHERE ranger_id NOT IN (
+    SELECT DISTINCT ranger_id FROM sightings
+);
+
+
+
+
+
+
+
+
+
 
 
 
